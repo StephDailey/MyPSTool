49681da7e57de5065da732f3999e0adf5e4c6a86
Steph Dailey <Stephaniedailey@my.unt.edu>
2021-04-30 14:28:58 -0500

    Added data.txt

---
+++ Remotescript.ps1
@@ -0,0 +1,15 @@
+﻿$session = New-PSSession -ComputerName 192.168.1.70 -Credential 7thheaven\Administrator
+
+try {
+Enter-PSSession $session 
+}
+
+catch {
+Write-host "Error: This action could not be completed." -ForegroundColor Red
+Write-host $_ 
+}
+
+finally {
+Write-host "Script is done." -ForegroundColor DarkYellow
+$computer = Read-host "Enter a computer name" 
+}
